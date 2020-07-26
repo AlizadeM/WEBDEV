52015244b9e2741557811083bc20627d6aff37a0
AlizadeM <61855009+AlizadeM@users.noreply.github.com>
2020-07-26 18:14:20 +0300

    .

---
+++ Omega_Slug/Heroes.js
@@ -0,0 +1,8 @@
+var photobig = document.getElementById("#testhover");
+var axe = document.getElementById("#axe");
+
+
+photobig.addEventListener("mouseover", function(){
+    axe.classList.add("axeAfter");
+});
+   
---
+++ Omega_Slug/home.js
@@ -0,0 +1,9 @@
+var download = document.getElementById("downButton");
+
+var thanks = document.getElementById("upDown");
+
+download.addEventListener("click", function(){ 
+        
+        thanks.textContent = "Thanks for downloading the game. Your download will start shortly!";
+        
+ });
